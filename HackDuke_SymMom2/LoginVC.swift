0d290cfa81b6effadf97f6b81e67eeec6164cd6e
William Luqiu <william.luqiu@duke.edu>
2020-12-05 16:34:54 -0500

    signin

--- HackDuke_SymMom2/LoginVC.swift
+++ HackDuke_SymMom2/LoginVC.swift
@@ -26,11 +26,29 @@ class LoginVC: UIViewController {
         
         print(username.text!)
         print(password.text!)
-        Auth.auth().createUser(withEmail: username.text!, password: password.text!) { user, error in
-          if error == nil {
-            // 3
-            Auth.auth().signIn(withEmail: self.username.text!,
-                               password: self.password.text!)
+        
+//        Auth.auth().createUser(withEmail: username.text!, password: password.text!) { user, error in
+//          if error == nil {
+//            // 3
+//            Auth.auth().signIn(withEmail: self.username.text!,
+//                               password: self.password.text!)
+//          }
+//        }
+        Auth.auth().signIn(withEmail: username.text!, password: password.text!) { user, error in
+          if let error = error, user == nil {
+            print(error)
+            let alert = UIAlertController(title: "Sign In Failed",
+                                          message: error.localizedDescription,
+                                          preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            
+            if var topController = UIApplication.shared.keyWindow?.rootViewController {
+                   while let presentedViewController = topController.presentedViewController {
+                       topController = presentedViewController
+                   }
+                   topController.present(alert, animated: true, completion: nil)
+            }
           }
         }
     }
