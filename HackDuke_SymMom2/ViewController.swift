99a663e71fdc61cf031cac07e95e5c37cd472ef5
William Luqiu <william.luqiu@duke.edu>
2020-12-05 13:25:42 -0500

    Initial Commit

---
+++ HackDuke_SymMom2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackDuke_SymMom2
+//
+//  Created by Billy Luqiu on 12/5/20.
+//  Copyright © 2020 Hack Duke Keohane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
