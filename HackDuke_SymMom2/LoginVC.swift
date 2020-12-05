77c65f47acc44fa5835087129309a90c18bd0101
David Li <david.li@duke.edu>
2020-12-05 15:13:02 -0500

    Stored login in LoginVC

---
+++ HackDuke_SymMom2/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  HackDuke_SymMom2
+//
+//  Created by David Li on 12/5/20.
+//  Copyright © 2020 Hack Duke Keohane. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginAction(_ sender: Any) {
+        print(username.text)
+        print(password.text)
+    }
+
+}
