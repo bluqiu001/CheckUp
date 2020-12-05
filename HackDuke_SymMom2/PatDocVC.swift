f456bc4e13ef32441c5c125a8ebc270a40778f3b
David Li <david.li@duke.edu>
2020-12-05 17:23:46 -0500

    Separated patient and doctor screens

--- HackDuke_SymMom2/PatDocVC.swift
+++ HackDuke_SymMom2/PatDocVC.swift
@@ -12,18 +12,6 @@ class PatDocVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)  {
-        if segue.identifier == "patientSegue" {
-            let loginVC = segue.destination as? LoginVC
-            loginVC?.isDoctor = false
-        }
-        else if segue.identifier == "doctorSegue" {
-            let loginVC = segue.destination as? LoginVC
-            loginVC?.isDoctor = true
-        }
     }
 }
---
+++ HackDuke_SymMom2/PatientLoginVC.swift
@@ -0,0 +1,56 @@
+//
+//  LoginVC.swift
+//  HackDuke_SymMom2
+//
+//  Created by David Li on 12/5/20.
+//  Copyright © 2020 Hack Duke Keohane. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class PatientLoginVC: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func loginAction(_ sender: Any) {
+        
+        print(username.text!)
+        print(password.text!)
+        
+//        Auth.auth().createUser(withEmail: username.text!, password: password.text!) { user, error in
+//          if error == nil {
+//            // 3
+//            Auth.auth().signIn(withEmail: self.username.text!,
+//                               password: self.password.text!)
+//          }
+//        }
+        Auth.auth().signIn(withEmail: username.text!, password: password.text!) { user, error in
+            if let error = error, user == nil {
+                print(error)
+                let alert = UIAlertController(title: "Sign In Failed",
+                                          message: error.localizedDescription,
+                                          preferredStyle: .alert)
+            
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+            
+                if var topController = UIApplication.shared.keyWindow?.rootViewController {
+                    while let presentedViewController = topController.presentedViewController {
+                        topController = presentedViewController
+                    }
+                topController.present(alert, animated: true, completion: nil)
+                }
+            }
+            else {
+                self.performSegue(withIdentifier: "PLoginMenuSegue", sender: self)
+            }
+        }
+    }
+
+}
